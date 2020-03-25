12249fb617d2944dcf4f88757175016559daee8b
stats <d>
2020-03-25 23:42:59 +0800

    加入跨域配置

---
+++ admin-auth/src/main/java/com/coin/auth/config/CorConfig.java
@@ -0,0 +1,11 @@
+package com.coin.auth.config;
+
+/**
+ * @ClassName CorConfig
+ * @Description: TODO
+ * @Author kh
+ * @Date 2020/3/24 16:28
+ * @Version V1.0
+ **/
+public class CorConfig {
+}
---
+++ admin-auth/src/main/java/com/coin/auth/config/shiro/CorFilter.java
@@ -0,0 +1,11 @@
+package com.coin.auth.config.shiro;
+
+/**
+ * @ClassName CorFilter
+ * @Description: TODO
+ * @Author kh
+ * @Date 2020/3/24 18:23
+ * @Version V1.0
+ **/
+public class CorFilter {
+}
