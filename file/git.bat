e939a699f343928909ca86bd3cc6a8beef7acf40
kh <dataanalyst@163.com>
2020-10-13 08:32:55 +0800

    git

---
+++ file/git.bat
@@ -0,0 +1,10 @@
+git bash
+
+git init
+
+git remote add origin https://github.com/DORcode/keycloak-11.0.1.git (如果报错 1、先输入 git remote rm origin
+2、再输入 git remote add origin**************)
+
+git add -A
+git commit -m "init"
+git push -u origin master
