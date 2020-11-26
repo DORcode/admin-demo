3bb80422556ad25127743aabbd18d8095dd24731
kh <dataanalyst@163.com>
2020-11-26 12:57:11 +0800

    修改

--- admin-base/src/main/java/com/coin/base/config/BaseController.java
+++ admin-base/src/main/java/com/coin/base/config/BaseController.java
@@ -1,8 +1,13 @@
 package com.coin.base.config;
 
+import com.google.common.collect.ImmutableMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @ClassName BaseController
@@ -18,4 +23,20 @@ public class BaseController {
     public String base() {
         return "base";
     }
+
+    private static List<Map<String, String>> books = Arrays.asList(
+            ImmutableMap.of("id", "book-1",
+                    "name", "Harry Potter and the Philosopher's Stone",
+                    "pageCount", "223",
+                    "authorId", "author-1"),
+            ImmutableMap.of("id", "book-2",
+                    "name", "Moby Dick",
+                    "pageCount", "635",
+                    "authorId", "author-2"),
+            ImmutableMap.of("id", "book-3",
+                    "name", "Interview with the vampire",
+                    "pageCount", "371",
+                    "authorId", "author-3")
+    );
+
 }
