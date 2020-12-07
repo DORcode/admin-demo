8059626880be63f56f3b62842791cba30eb7fb81
kh <dataanalyst@163.com>
2020-12-07 16:08:02 +0800

    db sharding

---
+++ db-sharding/db-sharding-sphere/src/main/java/com/coin/sphere/DbShardingSphereApplication.java
@@ -0,0 +1,13 @@
+package com.coin.sphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbShardingSphereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbShardingSphereApplication.class, args);
+    }
+
+}
