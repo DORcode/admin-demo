c36990d9071a4c8e5b6c3b10b4a7e6f86ddaae74
kh <dataanalyst@163.com>
2020-12-09 15:58:30 +0800

    sharding

--- db-sharding/db-sharding-sphere/src/test/java/com/coin/sphere/DbShardingSphereApplicationTests.java
+++ db-sharding/db-sharding-sphere/src/test/java/com/coin/sphere/DbShardingSphereApplicationTests.java
@@ -1,9 +1,15 @@
 package com.coin.sphere;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DbShardingSphereApplicationTests {
 
     @Test
